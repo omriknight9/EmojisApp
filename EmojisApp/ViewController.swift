e5f5f3cb90083184b0e366596d291d633e2031be
Omri Shalev <omriknight9@gmail.com>
2017-06-25 14:20:17 +0300

    Added a tableView

--- EmojisApp/ViewController.swift
+++ EmojisApp/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var emojiTableView: UITableView!
+    
+    let emojiArr = ["🤡", "💩", "🤖", "😈", "😱", "🤑", "🐯", "🎱"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiArr.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = emojiArr[indexPath.row]
+        return cell
+    }
+    
 
 }
 
